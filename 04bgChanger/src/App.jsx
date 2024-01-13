aae50a2759088a96e46c23b5a61ff6a9535261d9
Maanav-C <botbinary85@gmail.com>
2024-01-13 10:32:11 +0530

    05passwordgenerator Added

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+function App() {
+  const [color, setColor] = useState('Olive')
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
+            <button onClick={() => setColor("Red")} className="outline-none px-4 py-1 rounded-full text-lavender shadow-lg" style={{backgroundColor: 'Red'}}>Red</button>
+            <button onClick={() => setColor("Olive")} className="outline-none px-4 py-1 rounded-full text-lavender shadow-lg" style={{backgroundColor: 'Olive'}}>Olive</button>
+            <button onClick={() => setColor("Green")} className="outline-none px-4 py-1 rounded-full text-lavender shadow-lg" style={{backgroundColor: 'Green'}}>Green</button>
+            <button onClick={() => setColor("Blue")} className="outline-none px-4 py-1 rounded-full text-lavender shadow-lg" style={{backgroundColor: 'Blue'}}>Blue</button>
+            <button onClick={() => setColor("White")} className="outline-none px-4 py-1 rounded-full text-lavender shadow-lg" style={{backgroundColor: 'White'}}>White</button>
+
+          </div>
+         </div>
+      </div>
+    </>
+  )
+}
+
+export default App
